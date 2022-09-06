fc519046f802549bc9db63aef25e6b4046a948e5
bekzadsarybaev <bekzat97isakov>
2022-09-06 17:22:47 +0600

    commit is done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public Main()  {
     }
+
+    public static void main(String[] args) throws IOException {
+        FileWriter fileWriter = new FileWriter("Alfavit.txt");
+        fileWriter.write("A a \n");
+        fileWriter.write("B b \n");
+        fileWriter.write("C c \n");
+        fileWriter.write("... \n");
+        fileWriter.write("... \n");
+        fileWriter.write("Z z \n");
+        fileWriter.write("0 \n");
+        fileWriter.write("1 \n");
+        fileWriter.write("2 \n");
+        fileWriter.write("... \n");
+        fileWriter.write("9 \n");
+        fileWriter.close();
+        FileReader fileReader = new FileReader("Alfavit.txt");
+        Scanner scanner = new Scanner(fileReader);
+        while(scanner.hasNextLine()){
+            System.out.println(scanner.nextLine());
+
+            fileReader.close();
+        }
+    }
+
 }
